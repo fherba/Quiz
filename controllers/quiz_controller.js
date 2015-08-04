648e2ee4ec71ef95cfadfed5eaae54f9a9a10bf9
fherba <fherbag.mlist@gmail.com>
2015-08-04 23:44:48 +0200

    Añadir buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,9 +14,21 @@ exports.load = function (req, res, next, quizId) {
 
 //GET /quizes
 exports.index = function (req, res) {
-    models.Quiz.findAll().then ( function (quizes) {
-        res.render ( "quizes/index", { quizes: quizes });
-    }).catch ( function ( error ) { next ( error ); })
+    
+    if ( req.query.srch !== undefined ) {
+        req.query.srch = req.query.srch.replace ( / /g, "%");
+        models.Quiz.findAll( 
+            { order: "pregunta",
+              where: [ "pregunta LIKE ?", ( "%" + req.query.srch + "%" ) ] }
+        ).then ( function (quizes) {
+                res.render ( "quizes/index", { quizes: quizes });
+            }).catch ( function ( error ) { next ( error ); })
+    }
+    else {
+        models.Quiz.findAll().then ( function (quizes) {
+            res.render ( "quizes/index", { quizes: quizes });
+        }).catch ( function ( error ) { next ( error ); })
+    }
 };
 
 //GET /quizes/:id
