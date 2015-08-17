16addb782b778d0b14ee62ef17d0e3743add332c
fherba <fherbag.mlist@gmail.com>
2015-08-17 16:31:38 +0200

    Añadir creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -53,3 +53,24 @@ exports.answer = function (req, res) {
 exports.credits = function (req, res) {
     res.render ("author", { autor: "Francisco Herba" });
 };
+
+//GET /quizes/new
+
+exports.new = function ( req, res ) {
+    var quiz = models.Quiz.build ( //crea objeto quiz
+        { pregunta: "Pregunta", repuesta: "Respuesta" }
+    );
+    res.render ( "quizes/new", { quiz: quiz } );
+};
+
+//POST /quizes/create
+
+exports.create = function ( req, res ) {
+    var quiz = models.Quiz.build ( req.body.quiz );
+    
+    //guarda en DB los campos pregunta y respuesta de quiz
+    quiz.save ( { fields: ["pregunta", "respuesta" ]}).then ( function () {
+            res.redirect ( "/quizes" );
+    })
+};
+
